0698dce36d02bbf4ef7de6351bef59276b48b78e
Ali Gholami Rudi <aligrudi@users.sourceforge.net>
2007-12-31 21:58:36 +0330

    added copyright notice

--- ropemacs/__init__.py
+++ ropemacs/__init__.py
@@ -1,8 +1,22 @@
 """ropemacs, an emacs mode for using rope refactoring library"""
 
 import ropemacs.dialog
-import ropemacs.lisputils
 import ropemacs.interface
+import ropemacs.lisputils
+
+
+COPYRIGHT = """\
+Copyright (C) 2007-2008 Ali Gholami Rudi
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of GNU General Public License as published by the
+Free Software Foundation; either version 2 of the license, or (at your
+opinion) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details."""
 
 
 def _register_functions(interface):
