537a7529f9b0ab2d54a8c10a85810484c14bed17
Ali Gholami Rudi <aligrudi@users.sourceforge.net>
2008-01-24 22:15:44 +0330

    usefunction: added usefunction refactoring

--- ropemacs/refactor.py
+++ ropemacs/refactor.py
@@ -6,6 +6,7 @@ import rope.refactor.inline
 import rope.refactor.move
 import rope.refactor.rename
 import rope.refactor.restructure
+import rope.refactor.usefunction
 
 from ropemacs import dialog, lisputils
 
@@ -162,6 +163,19 @@ class Restructure(Refactoring):
             args=args, imports=imports)
         return restructuring.get_changes(resources=resources,
                                          task_handle=task_handle)
+
+
+class Restructure(Refactoring):
+    key = 'u'
+    optionals = {
+        'resources': dialog.Data('Files to apply this restructuring: ')}
+
+    def _calculate_changes(self, values, task_handle):
+        resources = _resources(self.project, values.get('resources', None))
+        user = rope.refactor.usefunction.UseFunction(
+            self.project, self.resource, self.offset)
+        return user.get_changes(resources=resources,
+                                task_handle=task_handle)
 
 
 class Move(Refactoring):
